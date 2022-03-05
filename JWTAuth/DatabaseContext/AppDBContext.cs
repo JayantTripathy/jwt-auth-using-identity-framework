f97a12f9c1a889bb50c6db791c1e6b233dc768a1
Jayant <jayanttripathy1985@gmail.com>
2022-03-06 00:20:56 +0530

    Add project files.

---
+++ JWTAuth/DatabaseContext/AppDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuth.DatabaseContext
+{
+    public class AppDBContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
